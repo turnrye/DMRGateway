b14822aff131f85c08ee0cd5a9699067eeb1adb4
Ryan Turner <ryan.e.t@gmail.com>
2016-03-29 12:48:47 -0500

    Combined dmr_install and install

--- install.sh
+++ install.sh
@@ -1,9 +1,31 @@
 #! /bin/sh
 
-apt-get install portaudio19-dev -y
+apt-get update
+apt-get install git portaudio19-dev sudo -y
 
-cp DMRGateway /usr/local/bin
+/srv/DMRGateway/DMRlink/mk_dmrlink
 
-cp DMRGateway.ini /etc
+# setup boot for DV3000
+systemctl stop getty@ttyAMA0.service
+systemctl disable getty@ttyAMA0.service
+cp /srv/DMRGateway/config.txt /boot
+cp /srv/DMRGateway/cmdline.txt /boot
 
+# Setup WiringPi
+/srv/DMRGateway/wiringPi/build
 
+# Setup AMBEserverGPIO
+make clean -C /srv/DMRGateway/OpenDV/DummyRepeater/DV3000
+make -C /srv/DMRGateway/OpenDV/DummyRepeater/DV3000
+make install -C /srv/DMRGateway/OpenDV/DummyRepeater/DV3000
+make init-install -C /srv/DMRGateway/OpenDV/DummyRepeater/DV3000
+python /srv/DMRGateway/OpenDV/DummyRepeater/DV3000/AMBEtest3.py
+cd /etc/init.d
+update-rc.d AMBEserverGPIO start 50 2 3 4 5
+
+# Setup DMRGateway
+cd /srv/DMRGateway/
+./install.sh
+
+ln -s /srv/DMRGateway/DMRGateway /usr/local/bin
+cp /srv/DMRGateway/DMRGateway.ini /etc
